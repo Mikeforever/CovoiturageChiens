461893ff2d7be115871ee0f86a7f3637b0525b82
MrMike <mikeloriginal@gmail.com>
2017-05-09 15:22:42 +0200

    Documents de base index et css

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Site de covoiturage pour le transport d'animaux</title>
+		<link rel="stylesheet" href="default.css" />
+	</head>
+	<body>
+		<header>
+			<h1>Bienvenue sur le site de covoiturage pour les animaux</h1>
+		</header>
+
+		<nav>
+			<!-- Les liens de navigation -->
+		</nav>
+
+		<section>
+			<!-- Les sections -->
+		</section>
+
+		<aside>
+			<!-- Pour des informations complémentaires sur le côté de la page -->
+		</aside>
+
+		<footer>
+			<!-- Le pied de page à renseigner -->
+		</footer>
+	</body>
+</html>
